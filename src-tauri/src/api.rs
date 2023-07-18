21f37f18b435ef61a1a373a9ef83b74d63dd1251
Brian Twene <57575362+briantwene@users.noreply.github.com>
2023-07-18 18:03:10 +0100

    Revert "Getting Image information (MVP)"

--- src-tauri/src/api.rs
+++
@@ -1,9 +0,0 @@
-
-
-//route for getting images
-fn images() {}
-
-//route for download
-fn download() {}
-//route for getting information
-fn info() {}
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import Navigation from "./components/Navigation";
-import { TanStackRouterDevtools } from "./utils/TanStackRouterDevTools";
-import { Outlet } from "@tanstack/router";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex h-screen font-poppins">
-      <Navigation />
-      <Outlet />
-      {/* <TanStackRouterDevtools /> */}
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
